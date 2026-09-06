ce7ffce22c1ea5b1a9ef19334b9cf6ef41071405
Maharshi Limbachiya <maharshi@openxcell.com>
2026-09-06 13:06:48 +0530

    add palindrome number and valid parentheses problems

---
+++ src/palindromenumber.java
@@ -0,0 +1,20 @@
+public class palindromenumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int reverse = 0;
+        int copy = x;
+        while (copy > 0) {
+            reverse = (reverse * 10) + copy % 10;
+            copy = copy / 10;
+        }
+        return x == reverse;
+    }
+}
---
+++ src/validparentheses.java
@@ -0,0 +1,53 @@
+public class validparentheses {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("([{}])"));
+    }
+
+    public static boolean isValid(String s) {
+        int length = s.length();
+
+        if (length % 2 == 1) {
+            return false;
+        }
+
+        char[] stack = new char[length / 2];
+
+        int pointer = -1;
+
+        char c1 = s.charAt(0);
+
+        if (c1 == ')' || c1 == '}' || c1 == ']') {
+            return false;
+        }
+
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            try {
+                if (c == ')') {
+                    if (stack[pointer] != '(') {
+                        return false;
+                    }
+                    pointer--;
+                } else if (c == '}') {
+                    if (stack[pointer] != '{') {
+                        return false;
+                    }
+                    pointer--;
+                } else if (c == ']') {
+                    if (stack[pointer] != '[') {
+                        return false;
+                    }
+                    pointer--;
+                } else {
+
+                    stack[++pointer] = c;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return false;
+            }
+        }
+
+        return pointer == -1;
+    }
+}
